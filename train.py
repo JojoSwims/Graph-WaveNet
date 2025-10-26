e8362086090203c74dc36e1a67a58a7f22c06b27
JojoSwims <62145111+JojoSwims@users.noreply.github.com>
2025-10-26 13:34:53 +0800

    Change the code to accept a config.json

--- train.py
+++ train.py
@@ -5,11 +5,12 @@ import time
 import util
 import matplotlib.pyplot as plt
 from engine import trainer
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--device',type=str,default='cuda:3',help='')
-parser.add_argument('--data',type=str,default='data/METR-LA',help='data path')
-parser.add_argument('--adjdata',type=str,default='data/sensor_graph/adj_mx.pkl',help='adj data path')
+parser.add_argument('--device',type=str,default='cuda:0',help='')
+parser.add_argument('--data',type=str,default='/dbfs/FileStore/johan/metrla/',help='data path')
+parser.add_argument('--adjdata',type=str,default='/dbfs/FileStore/johan/metrla/adj_mx.pkl',help='adj data path')
 parser.add_argument('--adjtype',type=str,default='doubletransition',help='adj type')
 parser.add_argument('--gcn_bool',action='store_true',help='whether to add graph convolution layer')
 parser.add_argument('--aptonly',action='store_true',help='whether only adaptive adj')
@@ -26,8 +27,10 @@ parser.add_argument('--weight_decay',type=float,default=0.0001,help='weight deca
 parser.add_argument('--epochs',type=int,default=100,help='')
 parser.add_argument('--print_every',type=int,default=50,help='')
 #parser.add_argument('--seed',type=int,default=99,help='random seed')
-parser.add_argument('--save',type=str,default='./garage/metr',help='save path')
+parser.add_argument('--save',type=str,default='/dbfs/FileStore/johan/runs/cluster2',help='save path')
 parser.add_argument('--expid',type=int,default=1,help='experiment id')
+parser.add_argument('--config', type=str, default=None, help='Path to JSON config')
+
 
 args = parser.parse_args()
 
@@ -39,6 +42,25 @@ def main():
     #torch.manual_seed(args.seed)
     #np.random.seed(args.seed)
     #load data
+
+    if args.config:
+        cfg_path = args.config
+        with open(cfg_path, 'r') as f:
+            cfg = json.load(f)
+        # Overwrite only keys present in JSON (preserve all other defaults)
+        for k, v in cfg.items():
+            if hasattr(args, k):
+                cur = getattr(args, k)
+                # light type-coercion to keep your types correct
+                if isinstance(cur, bool):
+                    v = bool(v)
+                elif isinstance(cur, int):
+                    v = int(v)
+                elif isinstance(cur, float):
+                    v = float(v)
+                else:
+                    v = str(v)
+                setattr(args, k, v)
     device = torch.device(args.device)
     sensor_ids, sensor_id_to_ind, adj_mx = util.load_adj(args.adjdata,args.adjtype)
     dataloader = util.load_dataset(args.data, args.batch_size, args.batch_size, args.batch_size)
@@ -51,7 +73,6 @@ def main():
         adjinit = None
     else:
         adjinit = supports[0]
-
     if args.aptonly:
         supports = None
 
